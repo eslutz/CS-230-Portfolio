09ee1690d1d49b63d27a894f63ed862a8186f665
Eric Slutz <eric.slutz@icloud.com>
2022-07-16 13:43:32 -0400

    created Entity class

---
+++ Project 1 - Draw It or Lose It/src/com/gamingroom/Entity.java
@@ -0,0 +1,44 @@
+package com.gamingroom;
+
+/**
+ * A simple class with id and name for use in classes
+ */
+public class Entity {
+    private long id;
+    private String name;
+
+    /**
+     * Hide the default constructor to prevent creating empty instances.
+     */
+    private Entity() {
+    }
+
+    /**
+     * Constructor with an identifier and name
+     */
+    public Entity(long id, String name) {
+        this();
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Game [id=" + id + ", name=" + name + "]";
+    }
+}
